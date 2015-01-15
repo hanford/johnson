a125fc7d47bcb17f692804024621d49e7f6adc0c
jack hanford <jackhanford@gmail.com>
2015-01-15 14:30:25 -0800

    better stylezzz

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,9 +4,13 @@
 
 angular.module('johnson.controllers', [])
 .controller('MainCtrl', function ($scope, $http) {
+  Waves.displayEffect();
+
+  $scope.query = "http://"
+
   $scope.search = function(query) {
     var search = {
-      'url': 'http://' + query
+      'url': query
     };
 
     $http.post('/api/geturl', search).then(function(response) {
